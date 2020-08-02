d3fe85463c0c1c968eafabfde61b0a719c59f674
HarigovindV10 <harigovindv.srt@gmail.com>
2020-08-03 01:54:46 +0530

    Version 1.0.0

--- JioFiBatteryChecker.py
+++ JioFiBatteryChecker.py
@@ -1,20 +1,18 @@
-#!/usr/bin/env python3
+#!/Library/Frameworks/Python.framework/Versions/3.7/bin python3
 
 from selenium import webdriver
 import time
 import os
-
-def notify(title, subtitle, message):
-    t = '-title {!r}'.format(title)
-    s = '-subtitle {!r}'.format(subtitle)
-    m = '-message {!r}'.format(message)
-    os.system('terminal-notifier {}'.format(' '.join([m, t, s])))
+from webdriver_manager.chrome import ChromeDriverManager
+from pync import Notifier
 
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 
-driver = webdriver.Chrome("/Users/harigovindvalsakumar/Downloads/chromedriver", options = options)
+driver = webdriver.Chrome(ChromeDriverManager().install(), options = options)
+
 try:
+
     driver.get("http://192.168.15.1/index.html")
 
     time.sleep(3)
@@ -30,11 +28,18 @@ try:
     battery_level = driver.find_element_by_xpath('//*[@id="batterylevel"]')
     charging_status = driver.find_element_by_xpath('//*[@id="batterystatus"]')
 
+    print(battery_level.text, charging_status.text)
+
     if(int(battery_level.text[0:2]) <= 20 and charging_status.text == 'Discharging'):
-        notify(title    = 'Battery Low',
-               subtitle = '',
-               message  = 'Please charge your JioFi')
+        Notifier.notify('Please charge your JioFi',
+                        title = 'Low Battery',
+                        subtitle = f'{battery_level.text} remaining ',
+                        sound = 'Ping',
+                        appIcon = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Reliance_Jio_Logo_%28October_2015%29.svg/1200px-Reliance_Jio_Logo_%28October_2015%29.svg.png')
+
 except:
+
+    driver.quit()
     exit()
 
 driver.quit()
